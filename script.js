b59171f3e4885ab5c5c9ed59ab01ed5ca2a99ec5
siyionyiampoi-IAB <siyionyiampoi@gmail.com>
2026-09-05 22:50:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-alert("WELCOME TO LYBRISYNC");
+document.addEventListener("DOMContentLoaded",function() {
+  const inventory=document.querySelector("#inventory");
+
+  if(inventory) {
+
+  inventory.addEventListener("click",function() {
+    alert("welcome to the lybrisync inventory!);
+          })
+  }
+});
+
 
